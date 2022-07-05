2c92dc9d28163b575f290ce78562a29b55012831
AndyNotfound <Andy.Notfound@gmail.com>
2022-07-05 20:28:34 +0700

    Section Browse Room

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../parts/Header'
 import Hero from '../parts/Hero'
+import BrowseRoom from '../parts/Home/BrowseRoom'
 
 export default function HomePage(props) {
   return (
     <>
-    <Header> </Header>
-    <Hero> </Hero>
+    <Header />
+    <Hero />
+    <BrowseRoom />
     </>
   )
 }
