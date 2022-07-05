5c72c75f8e55b694d77592e3cc8e88d0c3658d7d
AndyNotfound <Andy.Notfound@gmail.com>
2022-07-05 20:33:44 +0700

    Section Just Arrived

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../parts/Header'
 import Hero from '../parts/Hero'
 import BrowseRoom from '../parts/Home/BrowseRoom'
+import JustArrived from '../parts/Home/JustArrived'
 
 export default function HomePage(props) {
   return (
@@ -9,6 +10,7 @@ export default function HomePage(props) {
     <Header />
     <Hero />
     <BrowseRoom />
+    <JustArrived />
     </>
   )
 }
