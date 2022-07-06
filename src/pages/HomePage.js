a26d6b84865e9fa6a8aa87ae287fbbd6470ece14
AndyNotfound <Andy.Notfound@gmail.com>
2022-07-06 20:13:07 +0700

    Section Sitemap

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,7 @@ import Header from '../parts/Header'
 import Hero from '../parts/Hero'
 import BrowseRoom from '../parts/Home/BrowseRoom'
 import JustArrived from '../parts/Home/JustArrived'
+import Sitemap from '../parts/Sitemap'
 
 export default function HomePage(props) {
   return (
@@ -13,6 +14,7 @@ export default function HomePage(props) {
     <BrowseRoom />
     <JustArrived />
     <Client />
+    <Sitemap />
     </>
   )
 }
