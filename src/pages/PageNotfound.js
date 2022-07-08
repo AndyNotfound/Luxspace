4089d727c7b36401e2f87e6d16898cdbc39fe4f2
AndyNotfound <Andy.Notfound@gmail.com>
2022-07-08 21:37:52 +0700

    Notfound Component & Error page

---
+++ src/pages/PageNotfound.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ErrorPage from '../parts/ErrorPage'
+import Footer from '../parts/Footer'
+import Header from '../parts/Header'
+import Sitemap from '../parts/Sitemap'
+
+export default function Notfound() {
+  return (
+    <>
+    <Header />
+    <ErrorPage />
+    <Sitemap />
+    <Footer />
+    </>
+)
+}
