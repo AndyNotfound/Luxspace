172b3139ffa1b514de46e366911dcac49509a38a
AndyNotfound <Andy.Notfound@gmail.com>
2022-07-08 20:31:41 +0700

    Detail Page

---
+++ src/pages/Details.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Breadcrumb from '../components/Breadcrumb'
+import Client from '../parts/Client'
+import Footer from '../parts/Footer'
+import Header from '../parts/Header'
+import Sitemap from '../parts/Sitemap'
+
+export default function DetailPage() {
+  return (
+    <>
+    <Header theme="dark" />
+    <Breadcrumb list ={[
+      {url : '/', name: 'Home'},
+      {url : '/categories/91231', name : 'Office Room'},
+      {url : '/categories/91231/products/788', name : 'Details'}
+    ]}/>
+    <Sitemap />
+    <Footer />
+    </>
+  )
+}
