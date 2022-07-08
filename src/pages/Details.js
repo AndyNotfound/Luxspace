0dcda8570e3419d54ae9b0e7e572bf921f806b90
AndyNotfound <Andy.Notfound@gmail.com>
2022-07-08 20:40:42 +0700

    Suggestion section

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Breadcrumb from '../components/Breadcrumb'
 import Client from '../parts/Client'
 import Product from '../parts/Details/Product'
+import Suggestion from '../parts/Details/Suggestion'
 import Footer from '../parts/Footer'
 import Header from '../parts/Header'
 import Sitemap from '../parts/Sitemap'
@@ -16,6 +17,7 @@ export default function DetailPage() {
       {url : '/categories/91231/products/788', name : 'Details'}
     ]}/>
     <Product />
+    <Suggestion />
     <Sitemap />
     <Footer />
     </>
