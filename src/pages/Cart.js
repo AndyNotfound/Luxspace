dd33d3a7bd83a6beb4ac057c8e4510ca6c78617f
AndyNotfound <Andy.Notfound@gmail.com>
2022-07-08 21:03:46 +0700

    Cart page

---
+++ src/pages/Cart.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Breadcrumb from '../components/Breadcrumb'
+import Checkout from '../parts/Cart/Checkout'
+import Shipping from '../parts/Cart/Shipping'
+import Footer from '../parts/Footer'
+import Header from '../parts/Header'
+import Sitemap from '../parts/Sitemap'
+
+export default function Cart() {
+  return (
+    <>
+    <Header theme="dark" />
+    <Breadcrumb list ={[
+      {url : '/', name: 'Home'},
+      {url : '/cart', name : 'Shopping Cart'}
+    ]}/>
+
+    <section className="md:py-16">
+      <div className="container mx-auto px-4">
+        <div className="flex -mx-4 flex-wrap">
+          <Checkout />
+          <Shipping />
+        </div>
+      </div>
+    </section>
+
+    <Sitemap />
+    <Footer />
+    </>
+  )
+}
