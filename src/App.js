9ee87b7589bd929c2dc3333acc71f1512d8feef5
AndyNotfound <Andy.Notfound@gmail.com>
2022-07-08 21:38:17 +0700

    removing exact for the path

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import HomePage from './pages/HomePage';
 import './index.css'
 import DetailPage from './pages/Details';
 import Cart from './pages/Cart';
+import Congrats from './pages/Congrats';
+import PageNotfound from './pages/PageNotfound';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/categories/:idc" element={<DetailPage />} />
-        <Route exact path="/cart" element={<Cart />} />
+        <Route path="/categories/:idc" element={<DetailPage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/congratulation" element={<Congrats />} />
+        <Route path="*" element={<PageNotfound />} />
       </Routes>
     </BrowserRouter>
     
